7e7bdf8fd682dfb70404ea344787972247ecac51
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-12-13 17:35:45 -0500

    Add files via upload

--- ElectoralMap.java
+++ ElectoralMap.java
@@ -4,55 +4,104 @@ import java.lang.Exception;
 import java.util.ArrayList;
 import java.awt.Font;
 import java.awt.Color;
+import java.util.HashMap;
 
 public class ElectoralMap
 {   
-
+    HashMap<String, ArrayList<SubRegion>> region = new HashMap<>();
     //TO DO:
     //COMPELTE SUB REGION CLASS FIELD CONSTRUCTURO
     // ADD COMMENTS/NOTES TO ELECTORAL MAP CLASS
 
     // VERIFY THAT ELECTORAL MAP CAN STILL DRAW AN EMPTY MAP (ALSO GET POLYGON WORKING)
 
-    private class subRegion
+    private class SubRegion
     {
-        String name;
-        double[] xCoords;
-        double[] yCoords;
-        int[] votes;
-        public subRegion(String nameRegion, double[] xCoord, double[] yCoord, Color colorName, int[] voteArray)
+        private String name;
+        private double[] xCoords;
+        private double[] yCoords;
+        private int[] votes;
+        private Color col;
+        public SubRegion(String nameRegion, double[] xCoord, double[] yCoord)
         {
-            String name = nameRegion;
+            name = nameRegion;
             xCoords = xCoord;
             yCoords = yCoord;
-            votes = voteArray;
-            Color col = colorName;
+  
         }
 
-        public void changeName(String newName)
+        public void changeColor(Color newColor)
         {
-            name = newName;
+            color = newColor;
         }
 
         public void changeVote(int[] newVote)
         {
-            vote = newVote;
+            votes = newVote;
         }
-        
 
     }
-
-    
-    public static void votingFill()
+    public static void main()
     {
+        SubRegion testLocation = new SubRegion testLocation();
+        ArrayList<SubRegion> subregions = new ArrayList<>();
+        test.add(
+        region.put("Georgia", test);
+    }
+    public static void votingFill(String regionNameYear) throws Exception
+    {
+
+        int[] regionVotes = new int[3];
+
+        File f = new File("./input/"+regionNameYear+".txt");
+        Scanner inputObject = new Scanner(f);
+        inputObject.nextLine();   //Line1 lat/long min
+        //IGNORE STRING AT FIRST PART
+        while(inputObject.hasNextLine())
+        {
+            //SUB REGION VOTE 
+            String line = inputObject.nextLine();
+            String stringVotes[] = line.split(",");
+            regionVotes[0] = Integer.parseInt(stringVotes[1]);
+            regionVotes[1] = Integer.parseInt(stringVotes[2]);
+            regionVotes[2] = Integer.parseInt(stringVotes[3]);
+            double px = 0;
+            double py = 0;
+            double x = 0;
+            double y = 0;
+            int count = 0;
+            if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
+            {
+                //SUB REGION = RED
+            }
+            else if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
+            {
+                //SUB REGION = BLUE
+            }
+            else
+            {
+                //SUB REGION = GREEN
+            }
+            
+            //SUBREGION COLOR = DEPEND
+        }
+        //R = 0, D = 1, I = 2
+
+        
+        //    HashMap<String, ArrayList<SubRegion>> region = new HashMap<>();
+        //    Go through the ArrayList
+        //    Count all votes
+
+        //
         //Open voting data file
         //Fill each subRegion with voting data
-        //
+        //Read document and store info 
+        //3 Arrays
     }
 
     public static void coordsFill()
     {
-        
+        //USE HASHMAP HERE SINCE YOU NEED T OFILL ONE COLOR OR OTHER
         //fills xCoords and yCoords in each subRegion 
         //
         //(similar to emptyMap code) 
@@ -68,11 +117,9 @@ public class ElectoralMap
         //
 
     }
-    
+
     public static void visualize(String regionName) throws Exception
     {
-
-        //TAKE DATA FROM X AND Y COORDS
         String[] firstLine = new String[2];
         String[] secondLine = new String[2];
         File f = new File("./input/"+regionName+".txt");
@@ -80,7 +127,7 @@ public class ElectoralMap
         String line = inputObject.nextLine();   //Line1 lat/long min
         String line2 = inputObject.nextLine();  //Line2 lat/long max
         String line3 = inputObject.nextLine();  // # of subregions                                        //Line3 #subregions
-        //StdDraw.enableDoubleBuffering();
+        StdDraw.enableDoubleBuffering();
         double[] dub1 = new double[2];
         double[] dub2 = new double[2];
         firstLine = line.split("   ");
@@ -99,7 +146,7 @@ public class ElectoralMap
         int xSize = (int) (dub2[0] - dub1[0]);
         int ySize = (int) (dub2[1] - dub1[1]);
         int size = 0;
-       
+
         StdDraw.setCanvasSize(512*(xSize/ySize), 512);
         StdDraw.setXscale(dub1[0], dub2[0]);
         StdDraw.setYscale(dub1[1], dub2[1]); 
@@ -107,58 +154,55 @@ public class ElectoralMap
         StdDraw.circle(500, 500, 2000);
         StdDraw.point(500, 500);
         int i = 0;
+        boolean isFirst = true;
+        //inputObject.nextLine();
+        double[] xCoords;
+        double[] yCoords;
         while(inputObject.hasNextLine())
         {
-            double[] xCoords = new double[size];
-            double[] yCoords = new double[size];
-            String[] tempArr = inputObject.nextLine().split("   ");
-            
-            if(tempArr.length > 1)
+            ArrayList<SubRegion> subregions = new ArrayList<>();
+            //TO DO: define size at some point
+            //While loop witht the number of coordinates, not a flag. Know where scanner is at all time
+            //while has next line
+            inputObject.nextLine();
+            String tempName = inputObject.nextLine();
+            inputObject.nextLine();
+            size = Integer.parseInt(inputObject.nextLine());
+            count = 0;
+            xCoords = new double[size];
+            yCoords = new double[size];
+            //String[] tempArr = inputObject.nextLine().split("   ");
+
+            while(count < size)
             {
-                //TO DO: define size at some point
-                if(count > 0)
-                {
-                    px = x;
-                    py = y;
-                    
-                    x = Double.parseDouble(tempArr[0]);
-                    y = Double.parseDouble(tempArr[1]);
-                    xCoords[i] = x;
-                    yCoords[i] = y;
-                    i++;
-                    System.out.println("B");
-                    //StdDraw.line(px, py, x, y);
-                }
-                else
-                {
-                 xCoords = new double[size];
-                 yCoords = new double[size];
-                 
-                 x = Double.parseDouble(tempArr[0]);
-                 y = Double.parseDouble(tempArr[1]);
-                 //StdDraw.line(x, y, x, y);   
-                 
-                 count++;   
-                }
+                String[] tempArr = inputObject.nextLine().split("   ");
+                x = Double.parseDouble(tempArr[0]);
+                y = Double.parseDouble(tempArr[1]);
+                xCoords[count] = x;
+                yCoords[count] = y;
+                count++;
+
             }
-            else
-            {
-                inputObject.nextLine();
-                inputObject.nextLine();
             
-                size = Integer.parseInt(inputObject.nextLine());
+            SubRegion tempRegion = new SubRegion(tempName, xCoords, yCoords); //CREATE SUBREGION
+            subregions.add(tempRegion);         //ADD TEMPREGION TO ARRAYLIST
+            if(regions.containsKey(tempName))
+            {
+                //GET THE ARRAY LIST AND ITS VALUES. 
+                //ADD THOSE VALUES TO A NEW ARRAY LIST
+                //ADD NEW ARRAY LIST TO A NEW KEY
             }
-                count = 0;
-                System.out.println("A");
-                StdDraw.setPenColor(StdDraw.BLACK);
-                StdDraw.polygon(xCoords, yCoords);
-                StdDraw.show();
-            }
+            regions.put(tempName, subRegions);  //ADD TO HASH MAP
+            //YO SAM I'M WORKING DOWN HERE!!!!! THIS IS WHERE YOU LEFT OFF, SILLY WILLY! (left off up there^)
+            //Also conditional to see if the key already has a value, if it does take the value and add it back again 
+            //
+            StdDraw.polygon(xCoords, yCoords);
+
+        }
 
         StdDraw.show();
         inputObject.close();
 
-        
     }
 
 }
