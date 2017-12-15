45c7e600f094fbbbe4bfec7a5634f611dbf49925
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-12-15 16:52:12 -0500

    All Working

--- ElectoralMap.java
+++ ElectoralMap.java
@@ -5,65 +5,49 @@ import java.util.ArrayList;
 import java.awt.Font;
 import java.awt.Color;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.*;
 
 public class ElectoralMap
 {   
-    HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();    //
-    ArrayList<String> subRegionNames = new ArrayList<>(); //allows for hashmap traverse
-    //TO DO:
-    //COMPELTE SUB REGION CLASS FIELD CONSTRUCTURO
-    // ADD COMMENTS/NOTES TO ELECTORAL MAP CLASS
+    private static HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();   
 
-    // VERIFY THAT ELECTORAL MAP CAN STILL DRAW AN EMPTY MAP (ALSO GET POLYGON WORKING)
-    private int xSize = 0;
-    private int ySize = 0;
-    private  double[] dub1;
-    private double[] dub2;
-    private class SubRegion
+    private static class SubRegion
     {
+
         private String name;
         private double[] xCoords;
         private double[] yCoords;
         private int[] votes;
-        private Color col;
-        public SubRegion(String nameRegion, double[] xCoord, double[] yCoord)
+        private Color col = StdDraw.BLACK;
+        private double[] dub1;
+        private double[] dub2;
+        private SubRegion(String nameRegion, double[] xCoord, double[] yCoord)
         {
             name = nameRegion;
-            //visualize(nameRegion);
             xCoords = xCoord;
             yCoords = yCoord;
-  
         }
 
-        public void changeColor(Color newColor)
+        private void changeColor(Color newColor)
         {
             col = newColor;
         }
 
-        public void changeVote(int[] newVote)
+        private void changeVote(int[] newVote)
         {
             votes = newVote;
         }
 
     }
-    public void main(String regionName, String regionNameYear) throws Exception
+    public static void main(String regionName, String year) throws Exception
     {
         getGeoData(regionName);
-        votingFill(regionNameYear);
+        votingFill(regionName+year);
         draw();
-        //Geo data method
-        //Voting Fill
-        //Draw
-        
-        
-        //SubRegion testLocation = new SubRegion("GA");
-        //visualize("GA")
-        //votingFill("GA1980");
-        //ArrayList<SubRegion> subregions = new ArrayList<>();
-       // test.add(
-      //  region.put("Georgia", test);
     }
-    public void votingFill(String regionNameYear) throws Exception
+
+    public static void votingFill(String regionNameYear) throws Exception
     {
 
         int[] regionVotes = new int[3];
@@ -71,73 +55,50 @@ public class ElectoralMap
         File f = new File("./input/"+regionNameYear+".txt");
         Scanner inputObject = new Scanner(f);
         inputObject.nextLine();   //Line1 lat/long min
-        //IGNORE STRING AT FIRST PART
+
         while(inputObject.hasNextLine())
         {
-            //SUB REGION VOTE 
+
             String line = inputObject.nextLine();
             String stringVotes[] = line.split(",");
-            regionVotes[0] = Integer.parseInt(stringVotes[1]);
-            regionVotes[1] = Integer.parseInt(stringVotes[2]);
-            regionVotes[2] = Integer.parseInt(stringVotes[3]);
-            double px = 0;
-            double py = 0;
-            double x = 0;
-            double y = 0;
-            int count = 0;
-            System.out.println(regions.keySet());
+            regionVotes[0] = Integer.parseInt(stringVotes[1]); //Republican
+            regionVotes[1] = Integer.parseInt(stringVotes[2]); //Democrat
+            regionVotes[2] = Integer.parseInt(stringVotes[3]); //Independent
+
+            System.out.println("VotingFill: " + regions.keySet()); 
             System.out.println(stringVotes[0]);
-            //System.out.println(regions.get(stringVotes[0]).get(0));
-            ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);//THIS IS NULL
-            if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
+
+            ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
+
+            if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
             {
-                for(int i = 0; i < tempList.size(); i++)
+                for(int i = 0; i < tempList.size(); i++) //Go through arrayList of subregion obejcts
                 {
-                    tempList.get(i).changeColor(StdDraw.RED);
+                    tempList.get(i).changeColor(StdDraw.BLUE); //Change color of subRegion object to Blue
                 }
 
             }
-            else if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
+            else if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
             {
-                //ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
                 for(int i = 0; i < tempList.size(); i++)
                 {
-                    tempList.get(i).changeColor(StdDraw.BLUE);
+                    tempList.get(i).changeColor(StdDraw.RED); //Change color of subRegion object to Red
                 }
-                //return StdDraw.BLUE();
-                //SUB REGION = BLUE
+
             }
             else
-            {//
-                //ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
+            {
                 for(int i = 0; i < tempList.size(); i++)
                 {
                     tempList.get(i).changeColor(StdDraw.GREEN);
                 }
-                //return StdDraw.GREEN();
-                //SUB REGION = GREEN
             }
-            
-            //SUBREGION COLOR = DEPEND
         }
-        //R = 0, D = 1, I = 2
 
-        
-        //    HashMap<String, ArrayList<SubRegion>> region = new HashMap<>();
-        //    Go through the ArrayList
-        //    Count all votes
-
-        //
-        //Open voting data file
-        //Fill each subRegion with voting data
-        //Read document and store info 
-        //3 Arrays
     }
 
-
     
-
-    public void getGeoData(String regionName) throws Exception
+    public static void getGeoData(String regionName) throws Exception
     {
         String[] firstLine = new String[2];
         String[] secondLine = new String[2];
@@ -145,59 +106,54 @@ public class ElectoralMap
         Scanner inputObject = new Scanner(f);
         String line = inputObject.nextLine();   //Line1 lat/long min
         String line2 = inputObject.nextLine();  //Line2 lat/long max
-        String line3 = inputObject.nextLine();  // # of subregions                                        //Line3 #subregions
-        int totSub = Integer.parseInt(line3);
+        String line3 = inputObject.nextLine();                                        
+        int totSub = Integer.parseInt(line3);   //total numer of subregions
         StdDraw.enableDoubleBuffering();
         double[] dub1 = new double[2];
         double[] dub2 = new double[2];
+
         firstLine = line.split("   ");
         secondLine = line2.split("   ");
-        double px = 0;
-        double py = 0;
+        int count = 0;
         double x = 0;
         double y = 0;
-        int count = 0;
+
         for(int z = 0; z < firstLine.length; z++)
         {
             dub1[z] = Double.parseDouble(firstLine[z]);
             dub2[z] = Double.parseDouble(secondLine[z]);
         }
 
-        xSize = (int) (dub2[0] - dub1[0]);
-        ySize = (int) (dub2[1] - dub1[1]);
+        int xSize = (int) (dub2[0] - dub1[0]);
+        int ySize = (int) (dub2[1] - dub1[1]);
         int size = 0;
-
-        //StdDraw.setCanvasSize(512*(xSize/ySize), 512);
-        //StdDraw.setXscale(dub1[0], dub2[0]);
-        //StdDraw.setYscale(dub1[1], dub2[1]); 
-
-        //StdDraw.circle(500, 500, 2000);
-        //StdDraw.point(500, 500);
+        StdDraw.setCanvasSize(512*(xSize/ySize), 512);
+        StdDraw.setXscale(dub1[0], dub2[0]);
+        StdDraw.setYscale(dub1[1], dub2[1]);
+        
         int i = 0;
         boolean isFirst = true;
-        //inputObject.nextLine();
+
         double[] xCoords;
         double[] yCoords;
         int subCount = 0;
-        while(inputObject.hasNextLine() && subCount < totSub) //IT IS NOT CHE
+        while(inputObject.hasNextLine() && subCount < totSub) //While the end of the file hasn't been reached
         {
-            //
-            System.out.println(regions.keySet());
-            ArrayList<SubRegion> subregions = new ArrayList<>();
-            //TO DO: define size at some point
-            //While loop witht the number of coordinates, not a flag. Know where scanner is at all time
-            //while has next line
+            System.out.println("GeoData:"+ regions.keySet()); 
+            
+            ArrayList<SubRegion> subregions = new ArrayList<>();   
+
             inputObject.nextLine();
-            String subRegionName = inputObject.nextLine();   //USE TEMP NAME TO LOOK FOR FILL
-            System.out.println(subRegionName);
-            //inputObject.nextLine();
+            String subRegionName = inputObject.nextLine(); 
+            System.out.println("GeoData:"+ subRegionName);
+
             inputObject.nextLine();
             size = Integer.parseInt(inputObject.nextLine());
             count = 0;
             xCoords = new double[size];
             yCoords = new double[size];
-            //String[] tempArr = inputObject.nextLine().split("   ");
-                
+
+
             while(count < size)
             {
                 String[] tempArr = inputObject.nextLine().split("   ");
@@ -209,87 +165,56 @@ public class ElectoralMap
 
             }
             subCount++;
-            
-            ///WATCH OUT FOR THIS???? WHATS SUBREGION'S ROLE
-            //ADDS DATA TO A SUBREGION
-            SubRegion tempRegion = new SubRegion(subRegionName, xCoords, yCoords); //CREATE SUBREGION
-            
-            subregions.add(tempRegion);         //ADD TEMPREGION TO ARRAYLIST
-            ArrayList<SubRegion> tempList = new ArrayList<>();
-            if(regions.containsKey(subRegionName))
-            {
-                if(regions.get(subRegionName).size() > 1)
-                {
-                    while(regions.get(subRegionName).size() > 1) //If the array list has multiple values then you do all this
-                    {
-                        ArrayList<SubRegion> origList = regions.get(subRegionName);
-                        tempList.add(origList.get(regions.get(subRegionName).size() - 1));  //Add to templist
-                        //regions.remove(subRegionName);               //Remove from original list
-                       
-                    }
-                    System.out.println("AAAAAAAAAAAAAAAAAA");
-                    regions.put(subRegionName, tempList);
-   
-                }
-                else    //If the arrayList only has one calue 
-                {
-                System.out.println("AAAAAAAAAAAAAAAAAA");
-                subRegionNames.add(subRegionName);
-                tempList.add(regions.get(subRegionName).get(0));
-                regions.put(subRegionName, tempList);
-                //regions.get(tempName).get(0);
-                }
-                //GET THE ARRAY LIST AND ITS VALUES. 
-                //ADD THOSE VALUES TO A NEW ARRAY LIST
-                //ADD NEW ARRAY LIST TO A NEW KEY
-            }
-              //THIS SETS UP A KEY
-            //TEMPNAME GET SUBREGION FROM TEMPLIST AND GET THE COLOR
-            
-            //OR LOOP THROUGH AND FIND TEMPNAME 
-            //StdDraw.polygon(xCoords, yCoords);
 
+
+            SubRegion tempRegion = new SubRegion(subRegionName, xCoords, yCoords); //CREATE SUBREGION
+
+
+
+            if(regions.containsKey(subRegionName)) //IF THE KEY ALREADY EXISTS
+            {
+                    //if(regions.get(subRegionName).size() > 1) //If the array list has multiple values then you do all this
+                    //{
+                       // ArrayList<SubRegion> origList = regions.get(subRegionName);
+                       // tempList.add(origList.get(regions.get(subRegionName).size() - 1));  //Add to templist
+                        regions.get(subRegionName).add(tempRegion);
+
+                    //}
+
+                    regions.put(subRegionName, regions.get(subRegionName));
+            }
+                else    //If the arrayList only has one value 
+                {
+                    ArrayList<SubRegion> newList = new ArrayList<>();
+                    newList.add(tempRegion);
+                    regions.put(subRegionName, newList);
+       
+                }
         }
 
         StdDraw.show();
         inputObject.close();
 
     }
-    
-    public void draw()
-    {
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setCanvasSize(512*(xSize/ySize), 512);
-        StdDraw.setXscale(dub1[0], dub2[0]);
-        StdDraw.setYscale(dub1[1], dub2[1]); 
 
-        //TRAVERSE THROUGH HASHMAP
-        for(int i = 0; i < subRegionNames.size(); i++)
-        {
-           String subName = subRegionNames.get(i);
-           ArrayList<SubRegion> tempSubArray = regions.get(subName);
-           for(int z = 0; z < tempSubArray.size(); z++)
-           {
-               SubRegion tempSub = tempSubArray.get(z);
-               double[] tempXCoords = tempSub.xCoords;
-               double[] tempYCoords = tempSub.yCoords;
-               Color tempCol = tempSub.col;
-               StdDraw.setPenColor(tempCol);
-               StdDraw.filledPolygon(tempXCoords, tempYCoords);
-               StdDraw.setPenColor(StdDraw.BLACK);
-           }
-           
-            
-        }
+    public static void draw()
+    {
+        for(String key : regions.keySet())
+            {
+                for(SubRegion c : regions.get(key))
+                {
+                double[] tempXCoords = c.xCoords;
+                double[] tempYCoords = c.yCoords;
+                Color tempCol = c.col;
+                StdDraw.setPenColor(tempCol);
+                StdDraw.filledPolygon(tempXCoords, tempYCoords);
+                StdDraw.setPenColor(StdDraw.BLACK); 
+                System.out.println("Drawing: " + key); 
+                }
+                
+            }
         
-        //Call the visualize function, using the xCoords and yCoords for the polygons
-        //Use the votingFill() method to determine the color of the polygon
-        //Create polygons 
-        //Fill each area with colors using vote array (whichever vote has more)
-        //
-        //LOOPS THROUGH AND FINDS COLOR DATA AND X/Y DATA AND ACTUALLY MAKES POLYGONS
-        
-        //StdDraw.polygon(xCoords, yCoords);
+        StdDraw.show();
 
     }
 
--- ElectoraslMap.java
+++ ElectoraslMap.java
@@ -10,20 +10,14 @@ import java.util.*;
 
 public class ElectoraslMap
 {   
-    private static HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();    //
-    //private static ArrayList<String> subRegionNames = new ArrayList<>(); //allows for hashmap traverse
-    private static int xSize = 0;
-    private static int ySize = 0;
-    private static double[] dub1;
-    private static double[] dub2;
+    private static HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();   
     private static class SubRegion
     {
-
         private String name;
         private double[] xCoords;
         private double[] yCoords;
         private int[] votes;
-        private Color col;
+        private Color col = StdDraw.BLACK;
         private double[] dub1;
         private double[] dub2;
         private SubRegion(String nameRegion, double[] xCoord, double[] yCoord)
@@ -31,28 +25,21 @@ public class ElectoraslMap
             name = nameRegion;
             xCoords = xCoord;
             yCoords = yCoord;
-            
-
         }
-
+        
         private void changeColor(Color newColor)
-        {
-            col = newColor;
-        }
-
+        {   col = newColor;     }
+        
         private void changeVote(int[] newVote)
-        {
-            votes = newVote;
-        }
-
+        {   votes = newVote;    }
     }
+    
+   
     public static void main(String regionName, String year) throws Exception
     {
         getGeoData(regionName);
         votingFill(regionName+year);
         draw();
-
-   
     }
 
     public static void votingFill(String regionNameYear) throws Exception
@@ -63,34 +50,27 @@ public class ElectoraslMap
         File f = new File("./input/"+regionNameYear+".txt");
         Scanner inputObject = new Scanner(f);
         inputObject.nextLine();   //Line1 lat/long min
-        //IGNORE STRING AT FIRST PART
+
         while(inputObject.hasNextLine())
         {
-            //SUB REGION VOTE 
+
             String line = inputObject.nextLine();
             String stringVotes[] = line.split(",");
-            regionVotes[0] = Integer.parseInt(stringVotes[1]);
-            regionVotes[1] = Integer.parseInt(stringVotes[2]);
-            regionVotes[2] = Integer.parseInt(stringVotes[3]);
-            double px = 0;
-            double py = 0;
-            double x = 0;
-            double y = 0;
-            int count = 0;
-            System.out.println(regions.keySet());
-            System.out.println(stringVotes[0]);
-            //System.out.println(regions.get(stringVotes[0]).get(0));
-            ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);//THIS IS NULL
+            regionVotes[0] = Integer.parseInt(stringVotes[1]); //Republican
+            regionVotes[1] = Integer.parseInt(stringVotes[2]); //Democrat
+            regionVotes[2] = Integer.parseInt(stringVotes[3]); //Independent
+
+            //System.out.println("VotingFill: " + regions.keySet()); 
+            //System.out.println(stringVotes[0]);
+
+            ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
 
             if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
             {
-                System.out.println();
-                //ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
-                for(int i = 0; i < tempList.size(); i++)
+                for(int i = 0; i < tempList.size(); i++) //Go through arrayList of subregion obejcts
                 {
-                    tempList.get(i).changeColor(StdDraw.BLUE);
+                    tempList.get(i).changeColor(StdDraw.BLUE); //Change color of subRegion object to Blue
                 }
-                //SUB REGION = BLUE
             }
             else if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
             {
@@ -98,35 +78,18 @@ public class ElectoraslMap
                 {
                     tempList.get(i).changeColor(StdDraw.RED); //Change color of subRegion object to Red
                 }
-
             }
             else
-            {//
-                //ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
+            {
                 for(int i = 0; i < tempList.size(); i++)
                 {
                     tempList.get(i).changeColor(StdDraw.GREEN);
                 }
-                //return StdDraw.GREEN();
-                //SUB REGION = GREEN
             }
-
-            //SUBREGION COLOR = DEPEND
         }
-        //R = 0, D = 1, I = 2
 
-        //    HashMap<String, ArrayList<SubRegion>> region = new HashMap<>();
-        //    Go through the ArrayList
-        //    Count all votes
-
-        //
-        //Open voting data file
-        //Fill each subRegion with voting data
-        //Read document and store info 
-        //3 Arrays
     }
 
-    
     public static void getGeoData(String regionName) throws Exception
     {
         String[] firstLine = new String[2];
@@ -135,59 +98,49 @@ public class ElectoraslMap
         Scanner inputObject = new Scanner(f);
         String line = inputObject.nextLine();   //Line1 lat/long min
         String line2 = inputObject.nextLine();  //Line2 lat/long max
-        String line3 = inputObject.nextLine();  // # of subregions                                        //Line3 #subregions
-        int totSub = Integer.parseInt(line3);
+        String line3 = inputObject.nextLine();                                        
+        int totSub = Integer.parseInt(line3);   //total numer of subregions
         StdDraw.enableDoubleBuffering();
         double[] dub1 = new double[2];
         double[] dub2 = new double[2];
-
+        
         firstLine = line.split("   ");
         secondLine = line2.split("   ");
-        double px = 0;
-        double py = 0;
+        int count = 0;
         double x = 0;
         double y = 0;
-        int count = 0;
+        
         for(int z = 0; z < firstLine.length; z++)
         {
             dub1[z] = Double.parseDouble(firstLine[z]);
             dub2[z] = Double.parseDouble(secondLine[z]);
         }
 
-        xSize = (int) (dub2[0] - dub1[0]);
-        ySize = (int) (dub2[1] - dub1[1]);
+        int xSize = (int) (dub2[0] - dub1[0]);
+        int ySize = (int) (dub2[1] - dub1[1]);
         int size = 0;
         StdDraw.setCanvasSize(512*(xSize/ySize), 512);
         StdDraw.setXscale(dub1[0], dub2[0]);
         StdDraw.setYscale(dub1[1], dub2[1]);
 
-        //StdDraw.setCanvasSize(512*(xSize/ySize), 512);
-        //StdDraw.setXscale(dub1[0], dub2[0]);
-        //StdDraw.setYscale(dub1[1], dub2[1]); 
-
-        //StdDraw.circle(500, 500, 2000);
-        //StdDraw.point(500, 500);
         int i = 0;
         boolean isFirst = true;
-        //inputObject.nextLine();
+
         double[] xCoords;
         double[] yCoords;
         int subCount = 0;
-        while(inputObject.hasNextLine() && subCount < totSub) //IT IS NOT CHE
+        while(inputObject.hasNextLine() && subCount < totSub) //While the end of the file hasn't been reached
         {
-            System.out.println(regions.keySet());
-            ArrayList<SubRegion> subregions = new ArrayList<>();
-
+            //System.out.println("GeoData:"+ regions.keySet()); 
+            ArrayList<SubRegion> subregions = new ArrayList<>();   
             inputObject.nextLine();
-            String subRegionName = inputObject.nextLine();//.toUpperCase();  
-            System.out.println(subRegionName);
-
+            String subRegionName = inputObject.nextLine(); 
+            //System.out.println("GeoData:"+ subRegionName);
             inputObject.nextLine();
             size = Integer.parseInt(inputObject.nextLine());
             count = 0;
             xCoords = new double[size];
             yCoords = new double[size];
-
 
             while(count < size)
             {
@@ -200,33 +153,19 @@ public class ElectoraslMap
 
             }
             subCount++;
-
-
             SubRegion tempRegion = new SubRegion(subRegionName, xCoords, yCoords); //CREATE SUBREGION
-
-            subregions.add(tempRegion);         //ADD TEMPREGION TO ARRAYLIST
-            ArrayList<SubRegion> tempList = new ArrayList<>();
             if(regions.containsKey(subRegionName)) //IF THE KEY ALREADY EXISTS
-            {
-                    while(regions.get(subRegionName).size() > 1) //If the array list has multiple values then you do all this
-                    {
-                        ArrayList<SubRegion> origList = regions.get(subRegionName);
-                        tempList.add(origList.get(regions.get(subRegionName).size() - 1));  //Add to templist
-                        //regions.remove(subRegionName);               //Remove from original list
-
-                    }
-                    //System.out.println("AAAAAAAAAAAAAAAAAA");
-                    regions.put(subRegionName, tempList);
+            {                
+                regions.get(subRegionName).add(tempRegion);
+                regions.put(subRegionName, regions.get(subRegionName));
             }
-                else    //If the arrayList only has one value 
-                {
-                    //System.out.println("AAAAAAAAAAAAAAAAAA");
-                    tempList.add(tempRegion);
-                    regions.put(subRegionName, tempList);
-       
-                }
+            else    
+            {
+                ArrayList<SubRegion> newList = new ArrayList<>();
+                newList.add(tempRegion);
+                regions.put(subRegionName, newList);
+            }
         }
-
         StdDraw.show();
         inputObject.close();
 
@@ -234,45 +173,20 @@ public class ElectoraslMap
 
     public static void draw()
     {
-        /*String[] holdKeys = new String[pointsNum]; 
-        Set<String> key = regions.keySet();
-        holdKeys = key.toArray(holdKeys);
-        
-        for(int i = 0; i < holdKeys.length; i++)
-        {
-            holdKeys[i].get(); //OR TRY GOING THROUGH WHILE(NOTEMPTY) AND ANOTHER LOOPS
-        }
-        */
-       
-
-        
-        
-        //Issue with some counties not getting data filled
-        //maybe i'm not standardizing the name. Capitalized 
-        //String toUpperCase
-
-        System.out.println("XXXXXXX: " + regions.get("Macon"));
-        System.out.println("XXXXXXX2: " + regions.get("Franklin"));
-                
         for(String key : regions.keySet())
+        {
+            for(SubRegion c : regions.get(key))
             {
-                //String key = tempKey.toUpperCase();
-                for(SubRegion c : regions.get(key))
-                {
-                //SubRegion tempSub = tempSubArray.get(z);
                 double[] tempXCoords = c.xCoords;
                 double[] tempYCoords = c.yCoords;
                 Color tempCol = c.col;
                 StdDraw.setPenColor(tempCol);
                 StdDraw.filledPolygon(tempXCoords, tempYCoords);
                 StdDraw.setPenColor(StdDraw.BLACK); 
-                System.out.println(key); //Not getting processed.
-                }
-                
+                //System.out.println("Drawing: " + key); 
             }
-        
+        }
         StdDraw.show();
-
     }
 
 }
