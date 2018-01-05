2f54745c2b43d293beba0a874fa030823b905e85
Same Basket <30935970+sambaskin@users.noreply.github.com>
2018-01-05 10:02:44 -0500

    Electoral Map Mostly Working
    Louisiana forfeited accurate representation when they decided to be French.

--- ElectoralMap.java
+++ ElectoralMap.java
@@ -10,8 +10,11 @@ import java.util.*;
 
 public class ElectoralMap
 {   
-    private static HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();   
-
+    //Outer dude. Find GA
+    //Just get county voting data, don't even need subregions.
+    //
+    //private static HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();   
+    private static HashMap<String, HashMap<String, ArrayList<SubRegion>>> usa = new HashMap<>();
     public static class SubRegion
     {
 
@@ -40,59 +43,74 @@ public class ElectoralMap
         }
 
     }
-    
 
-    public static void votingFill(String regionNameYear) throws Exception
+    public static void votingFill(String year) throws Exception
     {
+        //get keys and loop over by calling keysety
+        //I only need to pass year first time
 
-        int[] regionVotes = new int[3];
+        for(String state: usa.keySet()){ 
+            int[] regionVotes = new int[3];
 
-        File f = new File("./input/"+regionNameYear+".txt");
-        Scanner inputObject = new Scanner(f);
-        inputObject.nextLine();   //Line1 lat/long min
+            File f = new File("./input/"+state+year+".txt");
+            Scanner inputObject = new Scanner(f);
+            inputObject.nextLine();   //Line1 lat/long min
 
-        while(inputObject.hasNextLine())
-        {
-
-            String line = inputObject.nextLine();
-            String stringVotes[] = line.split(",");
-            regionVotes[0] = Integer.parseInt(stringVotes[1]); //Republican
-            regionVotes[1] = Integer.parseInt(stringVotes[2]); //Democrat
-            regionVotes[2] = Integer.parseInt(stringVotes[3]); //Independent
-
-            System.out.println("VotingFill: " + regions.keySet()); 
-            System.out.println(stringVotes[0]);
-
-            ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
-
-            if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
+            while(inputObject.hasNextLine())
             {
-                for(int i = 0; i < tempList.size(); i++) //Go through arrayList of subregion obejcts
-                {
-                    tempList.get(i).changeColor(StdDraw.BLUE); //Change color of subRegion object to Blue
-                }
 
-            }
-            else if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
-            {
-                for(int i = 0; i < tempList.size(); i++)
-                {
-                    tempList.get(i).changeColor(StdDraw.RED); //Change color of subRegion object to Red
-                }
+                String line = inputObject.nextLine();
+                String stringVotes[] = line.split(",");
+                regionVotes[0] = Integer.parseInt(stringVotes[1]); //Republican
+                regionVotes[1] = Integer.parseInt(stringVotes[2]); //Democrat
+                regionVotes[2] = Integer.parseInt(stringVotes[3]); //Independent
 
-            }
-            else
-            {
-                for(int i = 0; i < tempList.size(); i++)
+                // System.out.println("VotingFill: " + regions.keySet()); 
+                
+
+                //ArrayList<SubRegion> tempList = regions.get(stringVotes[0]);
+                //System.out.println("DEBUG");
+                //System.out.println(usa.get(state));
+                System.out.println(stringVotes[0]);
+                System.out.println(state);
+                if(usa.get(state).containsKey(stringVotes[0]))
                 {
-                    tempList.get(i).changeColor(StdDraw.GREEN);
+                    
+                
+                
+                ArrayList<SubRegion> tempList = usa.get(state).get(stringVotes[0]); //ArrayList of subRegions
+                //System.out.println(tempList);
+
+                
+                if(regionVotes[1] > regionVotes[0] && regionVotes[1] > regionVotes[2])
+                {
+                    for(int i = 0; i < tempList.size(); i++) //Go through arrayList of subregion obejcts
+                    {
+                        tempList.get(i).changeColor(StdDraw.BLUE); //Change color of subRegion object to Blue
+                    }
+
+                }
+                else if(regionVotes[0] > regionVotes[1] && regionVotes[0] > regionVotes[2])
+                {
+                    for(int i = 0; i < tempList.size(); i++)
+                    {
+                        tempList.get(i).changeColor(StdDraw.RED); //Change color of subRegion object to Red
+                    }
+
+                }
+                else
+                {
+                    for(int i = 0; i < tempList.size(); i++)
+                    {
+                        tempList.get(i).changeColor(StdDraw.GREEN);
+                    }
                 }
             }
+        }
         }
 
     }
 
-    
     public static void getGeoData(String regionName) throws Exception
     {
         String[] firstLine = new String[2];
@@ -125,7 +143,7 @@ public class ElectoralMap
         StdDraw.setCanvasSize(512*(xSize/ySize), 512);
         StdDraw.setXscale(dub1[0], dub2[0]);
         StdDraw.setYscale(dub1[1], dub2[1]);
-        
+
         int i = 0;
         boolean isFirst = true;
 
@@ -134,20 +152,36 @@ public class ElectoralMap
         int subCount = 0;
         while(inputObject.hasNextLine() && subCount < totSub) //While the end of the file hasn't been reached
         {
-            System.out.println("GeoData:"+ regions.keySet()); 
-            
+            //DISCARD PARISH OR CITY OR COUNTY FROM SUBANME. CONDITIONAL GOES HERE
+
             ArrayList<SubRegion> subregions = new ArrayList<>();   
 
             inputObject.nextLine();
             String subRegionName = inputObject.nextLine(); 
             System.out.println("GeoData:"+ subRegionName);
 
-            inputObject.nextLine();
+            String outerRegionName = inputObject.nextLine();
             size = Integer.parseInt(inputObject.nextLine());
+            if(subRegionName.contains("city") || subRegionName.contains("county") || subRegionName.contains("Parish"))
+            {
+                int removeIndex = subRegionName.length(); 
+                if(subRegionName.indexOf("city") > subRegionName.indexOf("county") && subRegionName.indexOf("city") > subRegionName.indexOf("Parish"))
+                {
+                    removeIndex = subRegionName.indexOf("city");
+                }
+                else if(subRegionName.indexOf("county") > subRegionName.indexOf("city") && subRegionName.indexOf("county") > subRegionName.indexOf("Parish"))
+                {
+                    removeIndex = subRegionName.indexOf("county");
+                }
+                else
+                {
+                    removeIndex = subRegionName.indexOf("Parish");
+                }
+                subRegionName = subRegionName.substring(removeIndex-1);
+            }
             count = 0;
             xCoords = new double[size];
             yCoords = new double[size];
-
 
             while(count < size)
             {
@@ -161,23 +195,51 @@ public class ElectoralMap
             }
             subCount++;
 
-
             SubRegion tempRegion = new SubRegion(subRegionName, xCoords, yCoords); //CREATE SUBREGION
-
-
-
-            if(regions.containsKey(subRegionName)) //IF THE KEY ALREADY EXISTS
+            SubRegion tempOuterRegion = new SubRegion(outerRegionName,xCoords, yCoords); 
+            if(usa.containsKey(outerRegionName))
             {
-                    regions.get(subRegionName).add(tempRegion);
-                    regions.put(subRegionName, regions.get(subRegionName));
-            }
+                if(usa.get(outerRegionName).containsKey(subRegionName)) //IF THE KEY ALREADY EXISTS
+                {
+                    usa.get(outerRegionName).get(subRegionName).add(tempRegion);
+                    //regions.get(subRegionName).add(tempRegion);
+                    usa.get(outerRegionName).put(subRegionName, usa.get(outerRegionName).get(subRegionName));
+
+                }
                 else    //If the arrayList only has one value 
                 {
                     ArrayList<SubRegion> newList = new ArrayList<>();
                     newList.add(tempRegion);
-                    regions.put(subRegionName, newList);
-       
+                    usa.get(outerRegionName).put(subRegionName, newList);
+
                 }
+            }   
+            else
+            {
+                
+                HashMap<String, ArrayList<SubRegion>> innerMap = new HashMap<>();
+                ArrayList<SubRegion> subRegionList = new ArrayList<>();
+                subRegionList.add(tempRegion);
+                innerMap.put(subRegionName, subRegionList);
+                usa.put(outerRegionName, innerMap);
+            }
+
+            /*if(usa.containsKey(outerRegionName))
+            {
+            //usa.get(STATE).get(COUNTY)
+            if(usa.get(outerRegionName).containsKey(subRegionName))
+            {
+            usa.get(outerRegionName).get(subRegionName).add(tempRegion);
+            //COllection of states - Collection of counties- 
+            }
+            else
+            {
+            usa.get(outerRegionName).add(//Fulton)
+            //Hashmap <string, arraylist>
+            }
+            //put(tempOuterRegion);
+            }
+             */
         }
 
         StdDraw.show();
@@ -185,32 +247,55 @@ public class ElectoralMap
 
     }
 
-    public static void draw()
+    public static void draw(String year) throws Exception
     {
-        for(String key : regions.keySet())
+
+        for(String state : usa.keySet()){ //State 
+            //votingFill(state.name, year);
+            for(String county : usa.get(state).keySet()) //County
             {
-                for(SubRegion c : regions.get(key))
+                //  votingFill(county.name, year);
+                for(SubRegion c : usa.get(state).get(county))
                 {
-                double[] tempXCoords = c.xCoords;
-                double[] tempYCoords = c.yCoords;
-                Color tempCol = c.col;
-                StdDraw.setPenColor(tempCol);
-                StdDraw.filledPolygon(tempXCoords, tempYCoords);
-                StdDraw.setPenColor(StdDraw.BLACK); 
-                System.out.println("Drawing: " + key); 
+                    //Each county has a color already. Here we just cycle through counties
+                    double[] tempXCoords = c.xCoords;
+                    double[] tempYCoords = c.yCoords;
+                    //Get state w/ same name + year
+                    //votingFill(c.name, year);
+                    Color tempCol = c.col;
+                    StdDraw.setPenColor(tempCol);
+                    StdDraw.filledPolygon(tempXCoords, tempYCoords);
+                    StdDraw.setPenColor(StdDraw.BLACK); 
+                    System.out.println("Drawing: " + county); 
                 }
-                
+
             }
-        
+        }          
+
         StdDraw.show();
 
     }
-    
+
     public static void main(String regionName, String year) throws Exception
     {
+        //regionName = usa-county
+        //HashMap usa = [usa-county geoData]
+        //getGeoData("HOW DO WE GET STATE FOR THIS?" GET THE NAME OF IT IN THE OTHER GEODATA SO IT CALLS THE "GA" ONCE IT SEES IT
+        //maybe like a for loop to go through all the stuff for votingFill and whatnot lol
+        //votingFill(
+        //usa.get(STATE).get(COUNTY)
         getGeoData(regionName);
-        votingFill(regionName+year);
-        draw();
+        votingFill(year);
+        draw(year);
+        //Have everything below the hashmap already set. Just need to be able to get subRegions
+
+        //Fill GeoData for usa (GA) and create hashmap  usa.get(STATE)
+        //That's the region hashmap. Just put that in.
+        //Find voting data  usa.get(GA).get(Fulton) = arrayList (cycle through arrayList and fill each with the color)
+
+        //Modifiyng in the geoData to get GA and FULTON GA goes into the USA hashmap and FULTON goes into the GA hashmap
+        //we're ignoring states and GA rn. Change it so we do the same as county but with a new thang ;) <3l
+
     }
 
 }
